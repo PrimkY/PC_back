bd5cc2ec1c89776ba33a730595a66df881bb18f4
PrimkY <fakeprimky@gmail.com>
2024-07-25 18:53:36 +0300

    feat: added packages of swagger and graphql. setup base template for docs in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('PC configurator')
+    .setDescription('Тут будет описание этой документации')
+    .setVersion('1.0')
+    .addTag('configurator')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5000);
 }
 bootstrap();
